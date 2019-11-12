52fc6c13aaf1f514547073a853c0eb6f331d9d3c
luoxiapeng <949123073@qq.com>
2019-11-12 16:53:21 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      count:0
-     }
-  }
-  render() { 
-    return ( 
-      <div>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={this.addCount.bind(this)}>Chlick me</button>
-      </div>
-     );
-  }
-  addCount(){
-    this.setState({
-      count:this.state.count+1
-    })
-  }
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { 
+//       count:0
+//      }
+//   }
+//   render() { 
+//     return ( 
+//       <div>
+//         <p>You clicked {this.state.count} times</p>
+//         <button onClick={this.addCount.bind(this)}>Chlick me</button>
+//       </div>
+//      );
+//   }
+//   addCount(){
+//     this.setState({
+//       count:this.state.count+1
+//     })
+//   }
+// }
+
+function App() {
+  const [count, setCount] = useState(0)
+  return(
+    <div>
+      <p>{count}</p>
+      <button onClick={()=>{setCount(count+1)}}>click me</button>
+    </div>
+  )
+  
 }
  
 export default App;
